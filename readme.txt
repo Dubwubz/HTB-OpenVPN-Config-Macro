6408dec40501bdb605ce011747206fe288ffd13b
Nathan Astley <22crazyfrog@gmail.com>
2024-09-27 18:18:41 -0800

    ADD vpn.sh, ADD readme.txt

---
+++ readme.txt
@@ -0,0 +1,5 @@
+Usage: ./vpn.sh to run the script, for use on kali subsystem for windows while doing Hack the Box Challenges.
+
+Notes: 
+- Will need to manually redownload vpn file from webpage for each use.
+- Will delete all old vpn config files in windows download folder each run.
---
+++ vpn.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+echo " "
+echo " "
+echo "++++++++++++++++++++++++++++"
+echo "| HTB OpenVPNTunnel Script |"
+echo "++++++++++++++++++++++++++++"
+sleep 1
+echo " "
+echo "WARNING removing downloads ending in .ovpn, press enter to continue"
+rm -f /mnt/c/Users/Owner/Downloads/* | grep *.ovpn
+sleep 1
+echo " "
+echo "WARNING make sure to redownload vpn config file, press enter to continue"
+read
+sleep 1
+echo " "
+echo "Getting target..."
+sleep 1
+Target=$(ls /mnt/c/Users/Owner/Downloads/*)
+vpn=$(basename ${Target})
+sleep 1
+echo " "
+echo "removing old tunnel..."
+rm -f /home/dubwubz/HTB/openvpnlab/*
+sleep 1
+echo " "
+echo "replacing tunnel..."
+mv $Target /home/dubwubz/HTB/openvpnlab/
+sleep 1
+echo " "
+echo "All done!"
+sudo openvpn $vpn
